25308951c5d7c618aa180e75b2873e575a6508e6
mcibique <mcibique@hotmail.com>
2017-02-11 00:49:12 +0000

    Updated performance tests for take function.

--- src/spec/Performance.spec.js
+++ src/spec/Performance.spec.js
@@ -7,7 +7,9 @@ describe('Performance', function () {
     customers = [
       { name: 'John', age: 15 },
       { name: 'Joe', age: 19 },
-      { name: 'Adele', age: 21 }
+      { name: 'Adele', age: 21 },
+      { name: 'Ben', age: 35 },
+      { name: 'Jane', age: 24 }
     ];
   });
 
@@ -55,32 +57,40 @@ describe('Performance', function () {
       describe('via Array.from()', function () {
         it('should execute conditions over each customer', function () {
           let filtered = Array.from(result);
-          expect(filtered.length).toBe(2);
-          expect(whereCondition).toHaveBeenCalledTimes(3);
+          expect(filtered.length).toBe(4);
+          expect(whereCondition).toHaveBeenCalledTimes(5);
+        });
+      });
+
+      describe('via spread operator', function () {
+        it('should execute callbacks over each customer', function () {
+          let filtered = [...result];
+          expect(filtered.length).toBe(4);
+          expect(whereCondition).toHaveBeenCalledTimes(5);
         });
       });
 
       describe('via toArray()', function () {
         it('should execute callbacks over each customer', function () {
           let filtered = result.toArray();
-          expect(filtered.length).toBe(2);
-          expect(whereCondition).toHaveBeenCalledTimes(3);
+          expect(filtered.length).toBe(4);
+          expect(whereCondition).toHaveBeenCalledTimes(5);
         });
       });
 
       describe('via count()', function () {
         it('should execute conditions over each customer', function () {
           let count = result.count();
-          expect(count).toBe(2);
-          expect(whereCondition).toHaveBeenCalledTimes(3);
+          expect(count).toBe(4);
+          expect(whereCondition).toHaveBeenCalledTimes(5);
         });
       });
 
       describe('via aggregate()', function () {
         it('should execute conditions over each customer', function () {
-          let aggregated = result.aggregate((prev, curr) => prev.name + ' | ' + curr.name);
-          expect(aggregated).toBe('Joe | Adele');
-          expect(whereCondition).toHaveBeenCalledTimes(3);
+          let aggregated = result.aggregate((prev, curr) => (prev.name || prev) + ' | ' + curr.name);
+          expect(aggregated).toBe('Joe | Adele | Ben | Jane');
+          expect(whereCondition).toHaveBeenCalledTimes(5);
         });
       });
     });
@@ -103,24 +113,32 @@ describe('Performance', function () {
       describe('via Array.from()', function () {
         it('should execute callbacks over each customer', function () {
           let selected = Array.from(result);
-          expect(selected).toEqual(['John', 'Joe', 'Adele']);
-          expect(selectCallback).toHaveBeenCalledTimes(3);
+          expect(selected).toEqual(['John', 'Joe', 'Adele', 'Ben', 'Jane']);
+          expect(selectCallback).toHaveBeenCalledTimes(5);
         });
       });
 
       describe('via toArray()', function () {
         it('should execute callbacks over each customer', function () {
           let selected = result.toArray();
-          expect(selected).toEqual(['John', 'Joe', 'Adele']);
-          expect(selectCallback).toHaveBeenCalledTimes(3);
+          expect(selected).toEqual(['John', 'Joe', 'Adele', 'Ben', 'Jane']);
+          expect(selectCallback).toHaveBeenCalledTimes(5);
+        });
+      });
+
+      describe('via spread operator', function () {
+        it('should execute callbacks over each customer', function () {
+          let selected = [...result];
+          expect(selected).toEqual(['John', 'Joe', 'Adele', 'Ben', 'Jane']);
+          expect(selectCallback).toHaveBeenCalledTimes(5);
         });
       });
 
       describe('via aggregate()', function () {
         it('should execute callbacks over each customer', function () {
           let aggregated = result.aggregate((prev, curr) => prev + ' | ' + curr);
-          expect(aggregated).toBe('John | Joe | Adele');
-          expect(selectCallback).toHaveBeenCalledTimes(3);
+          expect(aggregated).toBe('John | Joe | Adele | Ben | Jane');
+          expect(selectCallback).toHaveBeenCalledTimes(5);
         });
       });
     });
@@ -155,11 +173,11 @@ describe('Performance', function () {
         });
 
         it('should execute where condition for every customer', function () {
-          expect(whereCondition).toHaveBeenCalledTimes(3);
+          expect(whereCondition).toHaveBeenCalledTimes(5);
         });
 
         it('should execute select callback only for items filtered by where condition', function () {
-          expect(selectCallback).toHaveBeenCalledTimes(1);
+          expect(selectCallback).toHaveBeenCalledTimes(3);
         });
       });
     });
@@ -214,11 +232,100 @@ describe('Performance', function () {
       });
 
       it('should execute where condition until first customer matches', function () {
+        expect(whereCondition).toHaveBeenCalledTimes(5);
+      });
+
+      it('should execute select callback only for items filtered by where condition', function () {
+        expect(selectCallback).toHaveBeenCalledTimes(3);
+      });
+    });
+  });
+
+  describe('where + take + select', function () {
+    let selectCallback,
+        whereCondition,
+        takeCount;
+
+    beforeEach(function () {
+      selectCallback = jest.fn((customer) => customer.age);
+      whereCondition = jest.fn((customer) => customer.age > 18);
+      takeCount = 2;
+    });
+
+    describe('when iteration is performed', function () {
+      let result;
+
+      beforeEach(function () {
+        result = customers
+          .where(whereCondition)
+          .take(takeCount)
+          .select(selectCallback)
+          .toArray();
+      });
+
+      it('should execute where condition until number of matched customers is 2', function () {
         expect(whereCondition).toHaveBeenCalledTimes(3);
       });
 
       it('should execute select callback only for items filtered by where condition', function () {
         expect(selectCallback).toHaveBeenCalledTimes(2);
+      });
+    });
+  });
+
+  describe('where + take + where', function () {
+    let whereCondition1,
+        whereCondition2,
+        takeCount;
+
+    beforeEach(function () {
+      whereCondition1 = jest.fn((customer) => customer.name.startsWith('J'));
+      whereCondition2 = jest.fn((customer) => customer.age > 18);
+      takeCount = 2;
+    });
+
+    describe('when iteration is performed', function () {
+      let result;
+
+      beforeEach(function () {
+        result = customers
+          .where(whereCondition1)
+          .take(takeCount)
+          .where(whereCondition2)
+          .toArray();
+      });
+
+      it('should execute 1st where condition until number of matched customers is 2', function () {
+        expect(whereCondition1).toHaveBeenCalledTimes(2);
+      });
+
+      it('should execute 2nd where condition only for customer taken by take(2)', function () {
+        expect(whereCondition2).toHaveBeenCalledTimes(2);
+      });
+    });
+  });
+
+  describe('take + first', function () {
+    let selectCallback,
+        takeCount;
+
+    beforeEach(function () {
+      selectCallback = jest.fn(customer => customer);
+      takeCount = 3;
+    });
+
+    describe('when iteration is performed', function () {
+      let result;
+
+      beforeEach(function () {
+        result = customers
+          .select(selectCallback)
+          .take(takeCount)
+          .first();
+      });
+
+      it('should ignore the takeCount and execute selectCallback only once', function () {
+        expect(selectCallback).toHaveBeenCalledTimes(1);
       });
     });
   });
