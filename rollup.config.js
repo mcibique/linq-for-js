538002ca7dd6fb11d8df4c3d708c3f60caee7096
mcibique <mcibique@hotmail.com>
2017-02-19 23:10:48 +0000

    Added sourcemaps.

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
 import babel from 'rollup-plugin-babel';
 import nodeResolve from 'rollup-plugin-node-resolve';
+import sourcemaps from 'rollup-plugin-sourcemaps';
 import uglify from 'rollup-plugin-uglify';
 
 export default {
   entry: './index.js',
   format: 'umd',
-  plugins: [babel(), nodeResolve(), uglify()],
+  sourceMap: true,
+  plugins: [babel(), nodeResolve(), uglify(), sourcemaps()],
   dest: 'dist/linq-for-js.min.js'
 };
