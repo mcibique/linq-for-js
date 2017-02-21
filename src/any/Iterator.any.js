85604db1e8feb122ef3c19428eb335f51f07d3d9
mcibique <mcibique@hotmail.com>
2017-02-21 23:04:53 +0000

    Any() function should always use first().

--- src/any/Iterator.any.js
+++ src/any/Iterator.any.js
@@ -2,14 +2,5 @@ import Iterator from '../iterators/Iterator';
 import '../first';
 
 Iterator.prototype.any = function any(condition) {
-  if (condition) {
-    for (let item of this) {
-      if (condition(item)) {
-        return true;
-      }
-    }
-    return false;
-  } else {
-    return !!this.first();
-  }
+  return !!this.first(condition);
 };
